08279091e4cdf295ad2ad65e62fe7c1e6896cb83
Kate Lee <145916607+kaitlynhlee@users.noreply.github.com>
2025-02-17 11:04:34 -0800

    Update fibo.asm

--- fibo.asm
+++ fibo.asm
@@ -1,85 +1,28 @@
-;****************************************************************************************************************************
-;Program name: "Average Driving Time".  This program calculates the average driving speed and time it took to go a certain distance based on user input
-; Copyright (C) 2024  Kaitlyn Lee.          *
-;                                                                                                                           *
-;This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  *
-;version 3 as published by the Free Software Foundation.  This program is distributed in the hope that it will be useful,   *
-;but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See   *
-;the GNU General Public License for more details A copy of the GNU General Public License v3 is available here:             *
-;<https://www.gnu.org/licenses/>.                                                                                           *
-;****************************************************************************************************************************
-
-
-
-
-;========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
-;Author information
-;  Author name: Kaitlyn Lee
-;  Author email: kaitlynlee@csu.fullerton.edu
-;  CWID: 886374479
-;  Class: 240-03 Section 03
-;
-;Program information
-;  Program name: Average Driving Time
-;  Programming languages: One module in C, one in X86, and one in bash.
-;  Date program began: 2024-Jan-30
-;  Date of last update: 2024-Feb-4
-;  Files in this program: driver.c, average.asm, r.sh.
-;  Testing: Alpha testing completed.  All functions are correct.
-;  Status: Ready for release to customers
-;
-;Purpose
-;  This program is a driving time, speed, and distance calculator based on the user's input
-;
-;This file:
-;  File name: average.asm
-;  Language: X86-64
-;  Max page width: 124 columns
-;  Assemble (standard): nasm -f elf64 -l avg.lis -o avg.o average.asm
-;  Assemble (debug): nasm -f elf64 -gdwarf -l avg.lis -o avg.o average.asm
-;  Optimal print specification: Landscape, 7 points, monospace, 8½x11 paper
-;  Prototype of this function: unsigned long helloword();
-; 
-;
-;
-;
-;========1=========2=========3=========4=========5=========6=========7=========8=========9=========0=========1=========2=========3**
-
 ;declarations
 
 extern printf
-
 extern scanf
 
-extern cos
-
-extern sin
-
-extern tan
-
-global calculator
+global fibo
 
 string_size equ 48
+
 
 segment .data
 ;declare initialized arrays
 
-prompt_for_nums
-num
+prompt_for_nums db "type 5 numbers: ", 0
+num db "%lf", 0
+result db "array: ", 10, 0
+
 
 segment .bss
 ;declare empty arrays
 
-align 64
-backup_storage_area resb 832
-
-user_name resb string_size
-user_title resb string_size
 
 segment .text
 
-calculator:
-
+fibo:
 ;backup GPRs
 push rbp
 mov rbp, rsp
@@ -98,63 +41,80 @@ push r14
 push r15
 pushf
 
-;backup other registers
-mov rax,7
-mov rdx,0
-xsave [backup_storage_area]
+mov r11, 0
+mov r12, 0
+mov r13, 1
+mov r14, 0
 
+;prompt user for numbers
+mov rax, 0
+mov rdi, prompt_for_nums
+call printf
 
-;prompt user for nums
+fill:
+;cmp r11, 4
+;je sort
+;je end
 
-
-;input for num 1
-
-
-;input for num 2
-
-
-
-;add two nums
-
-  
-;subtract two nums
-
-  
-;multiply two nums
-
-
-;divide two nums
-
-
-;get square root of a number
-
-
-;get cos of a number
-
-
-;get sin of a number
-
-
-;get tan of a number
-
-  
-
-
-
-;move results to stack
+;get values
 mov rax, 0
 push qword 0
-movsd [rsp], xmm15 ;move average speed in xmm15 to stack to restore sse registers
-
-;Restore the values to non-GPRs
-mov rax,7
-mov rdx,0
-xrstor [backup_storage_area]
-
-;Send back average speed
-movsd xmm0, [rsp] ;move average speed from top of stack to xmm0 to send back to driver
+push qword 0
+mov rdi, num
+mov rsi, rsp
+call scanf
+movsd xmm15, [rsp]
+pop rax
 pop rax
 
+movsd [r15+r11*8], xmm15
+
+;inc r11
+;jmp fill
+
+;sort:
+;cmp r13, 5
+;je printing
+
+;cmpsd [r15+r12*8], [r15+r13*8]
+;jg swap
+
+;inc r12
+;inc r13
+
+;jmp sort
+
+;swap:
+;movsd xmm15, [r15+r12*8]
+;movsd [r15+r12*8], [r15+r13*8]
+;movsd [r15+r13*8], xmm15
+
+;inc r12
+;inc r13
+
+;jmp sort
+
+printing: 
+mov rax, 0
+mov rdi, result
+call printf
+
+;output:
+;cmp r14, 5
+;je end
+
+;mov rax, 1
+;mov rdi, num
+;movsd xmm0, [r15+r14*8]
+;call printf
+
+;inc r14
+;jmp output
+
+
+end: 
+
+mov rax, r11
 
 ;Restore the GPRs
 popf
