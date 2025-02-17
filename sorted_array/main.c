4bd91ce8fbb4e17d46839d8409523b0de1fee8e5
Kate Lee <145916607+kaitlynhlee@users.noreply.github.com>
2025-02-17 11:53:10 -0800

    Create main.c

---
+++ sorted_array/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+//declaration for external function
+extern void manager();
+
+int main(void)
+{
+//inform user about the program and author
+ printf("Welcome to Arrays of Floating Point Numbers.\n");
+ 
+ manager();
+
+ printf("Main will return 0 to the operating system. Bye.\n");
+}
