3ada952a0956bb405d53a0ee0c040cb7e91cd1f1
Kate Lee <145916607+kaitlynhlee@users.noreply.github.com>
2025-02-17 12:33:32 -0800

    Create swap.asm

---
+++ sorted_array/swap.asm
@@ -0,0 +1,55 @@
+;Declaration area
+
+global swap
+
+segment .data
+   ;This segment is empty
+
+segment .bss
+   ;This segment is empty
+
+segment .text
+swap:
+
+;Block that backs up almost all GPRs
+;Back up the general purpose registers for the sole purpose of protecting the data of the caller.
+push rbp                                          ;Backup rbp
+mov  rbp,rsp                                      ;The base pointer now points to top of stack
+push rdi                                          ;Backup rdi
+push rsi                                          ;Backup rsi
+push rdx                                          ;Backup rdx
+push rcx                                          ;Backup rcx
+push r8                                           ;Backup r8
+push r9                                           ;Backup r9
+push r10                                          ;Backup r10
+push r11                                          ;Backup r11
+push r12                                          ;Backup r12
+push r13                                          ;Backup r13
+push r14                                          ;Backup r14
+push r15                                          ;Backup r15
+push rbx                                          ;Backup rbx
+pushf                                             ;Backup rflags
+
+mov rdx, [rdi]
+mov rcx, [rsi]
+
+mov [rdi], rcx
+mov [rsi], rdx
+
+popf                                    ;Restore rflags
+pop rbx                                 ;Restore rbx
+pop r15                                 ;Restore r15
+pop r14                                 ;Restore r14
+pop r13                                 ;Restore r13
+pop r12                                 ;Restore r12
+pop r11                                 ;Restore r11
+pop r10                                 ;Restore r10
+pop r9                                  ;Restore r9
+pop r8                                  ;Restore r8
+pop rcx                                 ;Restore rcx
+pop rdx                                 ;Restore rdx
+pop rsi                                 ;Restore rsi
+pop rdi                                 ;Restore rdi
+pop rbp                                 ;Restore rbp
+
+ret                                     ;Pop the integer stack and jump to the address represented by the popped value.
